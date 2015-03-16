5d90c6613454f48724ac9a85a7a41481b393f120
Nick Salkowski <nicksalkowskiphd@gmail.com>
2015-03-16 15:08:43 -0500

    filter_pithy and select_pithy split off

---
+++ R/filter_pithy.R
@@ -0,0 +1,75 @@
+#' @title Filter Data Prior to Plotting
+#' 
+#' @description
+#' These functions call \code{\link[dplyr]{filter}} or 
+#' \code{\link[dplyr]{filter_}} before calling \code{\link{pithy}}.
+#' 
+#' @details
+#' These functions let data.frame, tbl, or tbl_df objects be modified 
+#' prior to producing plots with \code{\link{pithy}}.  Just like pithy, 
+#' these functions return the unmodified data set.  \code{filter_pithy} 
+#' calls \code{\link[dplyr]{filter}} prior to calling \code{\link{pithy}}. 
+#' \code{filter_pithy_} calls \code{\link[dplyr]{filter_}} prior to 
+#' calling \code{\link{pithy}}.  
+#' 
+#' @param .data A data.frame, tbl, or tbl_df
+#' @param ... Additional arguments to pass to 
+#' \code{\link[dplyr]{filter}} or 
+#' \code{\link[dplyr]{filter_}},
+#' @param .pithargs List of arguments to pass to \code{\link{pithy}}.
+#' @examples
+#' # filter_pithy -----
+#' head(filter_pithy(cars, speed > 10))
+#' 
+#' @name filter_pithy
+#' @export
+filter_pithy <- function(.data, ..., .pithargs = list()) {
+  
+  if (is.null(.pithargs$xname)) {
+    xname <- deparse(substitute(.data))
+  }
+  
+  filterargs <- deparse(substitute(list(...)))
+  xname <- paste0(
+    "filter(", 
+    xname, 
+    ", ", 
+    substr(filterargs, 6, nchar(filterargs)), 
+    "\n")
+  
+  .pithargs$x <- dplyr::filter(.data, ...)
+  .pithargs$xname <- xname
+  
+  do.call(
+    what = pithy,
+    args = .pithargs)
+  
+  return(.data)
+}
+
+#' @rdname filter_pithy
+#' @export
+filter_pithy_ <- function(.data, ..., .pithargs = list()) {
+  
+  if (is.null(.pithargs$xname)) {
+    xname <- deparse(substitute(.data))
+  }
+  
+  filterargs <- deparse(substitute(list(...)))
+  xname <- paste0(
+    "filter_(", 
+    xname, 
+    ", ", 
+    substr(filterargs, 6, nchar(filterargs)), 
+    "\n")
+  
+  .pithargs$x <- dplyr::filter_(.data, ...)
+  .pithargs$xname <- xname
+  
+  do.call(
+    what = pithy,
+    args = .pithargs)
+  
+  return(.data)
+}
+
---
+++ R/select_pithy.R
@@ -0,0 +1,57 @@
+#' @title Select Data Prior to Plotting
+#' 
+#' @description
+#' These functions call \code{\link[dplyr]{select}} or 
+#' \code{\link[dplyr]{select_}} before calling \code{\link{pithy}}.
+#' 
+#' @details
+#' These functions let data.frame, tbl, or tbl_df objects be modified 
+#' prior to producing plots with \code{\link{pithy}}.  Just like pithy, 
+#' these functions return the unmodified data set.  \code{select_pithy} 
+#' calls \code{\link[dplyr]{select}} prior to calling \code{\link{pithy}}. 
+#' \code{select_pithy_} calls \code{\link[dplyr]{select_}} prior to 
+#' calling \code{\link{pithy}}.  
+#' 
+#' @param .data A data.frame, tbl, or tbl_df
+#' @param ... Additional arguments to pass to 
+#' \code{\link[dplyr]{select}} or 
+#' \code{\link[dplyr]{select_}},
+#' @param .pithargs List of arguments to pass to \code{\link{pithy}}.
+#' @examples
+#' # select_pithy -----
+#' head(select_pithy(cars, dist))
+#' @name select_pithy
+#' @export
+select_pithy <- function(.data, ..., .pithargs = list()) {
+  
+  if (is.null(.pithargs$xname)) {
+    xname <- deparse(substitute(.data))
+  }
+  
+  .pithargs$x <- dplyr::select(.data, ...)
+  .pithargs$xname <- xname
+  
+  do.call(
+    what = pithy,
+    args = .pithargs)
+  
+  return(.data)
+}
+
+#' @rdname select_pithy
+#' @export
+select_pithy_ <- function(.data, ..., .pithargs = list()) {
+  
+  if (is.null(.pithargs$xname)) {
+    xname <- deparse(substitute(.data))
+  }
+  
+  .pithargs$x <- dplyr::select_(.data, ...)
+  .pithargs$xname <- xname
+  
+  do.call(
+    what = pithy,
+    args = .pithargs)
+  
+  return(.data)
+}
