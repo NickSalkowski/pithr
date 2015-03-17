df479532dddd5d5336a63a395e7980fce9957ed4
Nick Salkowski <nicksalkowskiphd@gmail.com>
2015-03-17 11:25:58 -0500

    Code cleanup on plot.pith

--- R/plot.pith.R
+++ R/plot.pith.R
@@ -1,14 +1,21 @@
-#' @title Plot a \code{pith} class object
-#' 
+#' @title Plot a pith class object
+#'
 #' @param x A pith.
-#' @param freq Logical.  If \code{TRUE}, frequencies will be plotted instead of proportions or densities.
+#' @param freq Logical.  If \code{TRUE}, frequencies will be plotted instead of
+#' proportions or densities.
 #' @param main Character string. Main title, default is \code{x$xname}
 #' @param xlab Character string. X-axis label.
 #' @param ylab Character string. Y-axis label.
-#' @param col Character vector.  Up to two fill colors may be specified, the first for the nonmissing data bars and the second for the NA bar.  If only one color is specified it will be used for all bars.
-#' @param border Character vector.  Up to two border colors may be specified, the first for the nonmissing data bars and the second for the NA bar.  If only one color is specified it will be used for all bars.
-#' @param las numeric in {0, 1, 2, 3}. Axis label style.  See \code{\link[graphics]{par}}
-#' @param shrink Logical.  If TRUE, factor labels will shrink to fit the width of the bar.  Ignored 
+#' @param col Character vector.  Up to two fill colors may be specified, the
+#' first for the nonmissing data bars and the second for the NA bar.  If only
+#' one color is specified it will be used for all bars.
+#' @param border Character vector.  Up to two border colors may be specified,
+#' the first for the nonmissing data bars and the second for the NA bar.  If
+#' only one color is specified it will be used for all bars.
+#' @param las numeric in {0, 1, 2, 3}. Axis label style.  See
+#' \code{\link[graphics]{par}}
+#' @param shrink Logical.  If TRUE, factor labels will shrink to fit the width
+#' of the bar.  Ignored 
 #' if \code{las} is either 2 or 3.
 #' @param ... Additional plot parameters.
 #' @method plot pith
@@ -16,48 +23,48 @@
 #' @return \code{NULL}, invisibly.
 
 plot.pith <- function(
-  x, 
-  freq = TRUE, 
-  main = NULL, 
-  xlab = NULL, 
-  ylab = NULL, 
-  col = c("#619CFF", "#F8766D"), 
-  border = NA, 
+  x,
+  freq = TRUE,
+  main = NULL,
+  xlab = NULL,
+  ylab = NULL,
+  col = c("#619CFF", "#F8766D"),
+  border = NA,
   las = 1,
   shrink = TRUE,
   ...) {
-  
+
   col <- rep(col, length.out = 2)
   border <- rep(border, length.out = 2)
-  
+
   for (i in seq_along(x)) {
-    
+
     xname <- x[[i]]$xname
-    
+
     if (is.null(main)) {
       main <- xname
     }
-    
+
     xfreq <- x[[i]]$freq
     xhist <- x[[i]]$hist
-    
+
     if (!is.null(xfreq)) {
-      
+
       if (is.null(ylab)) {
         ylab <- ifelse(freq, "Frequency", "Proportion")
       }
-      
+
       pheight <- if (freq) {
         xfreq$x_freq
       } else {
         xfreq$x_prop
       }
-      
+
       pcol <- rep(col[1], length.out = length(pheight))
       pborder <- rep(border[1], length.out = length(pheight))
-      
+
       pnames <- xfreq$x
-      
+
       if (xfreq$NA_freq != 0L) {
         naheight <- if (freq) {
           xfreq$NA_freq
@@ -69,7 +76,7 @@ plot.pith <- function(
         pcol <- c(pcol, NA, col[2])
         pborder <- c(pborder, NA, border[2])
       }
-      
+
       bp <- graphics::barplot(
         height = pheight,
         names.arg = pnames,
@@ -81,7 +88,7 @@ plot.pith <- function(
         border = pborder,
         las = las,
         ...)
-      
+
       if (shrink & las %in% c(0, 1)) {
         pparms <- list(...)
         c.a <- c(pparms$cex.axis, par("cex.axis"))[1]
@@ -93,44 +100,44 @@ plot.pith <- function(
             tick = FALSE,
             labels = pnames[j],
             cex.axis = ifelse(
-              s_width <= 1.0, 
-              c.a, 
+              s_width <= 1.0,
+              c.a,
               c.a / s_width))
         }
       } else {
         axis(side = 1, at = bp, labels = pnames, las = las, tick = FALSE)
       }
     }
-    
+
     if (!is.null(xhist)) {
-      
+
       if (is.null(las)) {
         las <- 1
       }
-      
+
       if (is.null(xlab)) {
         xlab <- ""
       }
-      
+
       if (is.null(ylab)) {
         ylab <- ifelse(freq, "Frequency", "Density")
       }
-      
+
       brange <- range(xhist$hist$breaks)
-      
+
       bdiff <- mean(diff(xhist$hist$breaks))
-      
+
       xlim <- brange
       phist <- xhist$hist
-      
+
       which_checks <- c(
-        xhist$Inf_freq != 0L, 
+        xhist$Inf_freq != 0L,
         xhist$NA_freq != 0L)
-      
+
       n_checks <- sum(which_checks)
-      
+
       which_checks <- which(which_checks)
-      
+
       if (n_checks > 0L) {
         xlim[2] <- xlim[2] + (1 + n_checks * 1.25) * bdiff
         phist$density <- phist$density * (1 - xhist$NA_prop - xhist$Inf_prop)
@@ -143,31 +150,31 @@ plot.pith <- function(
           c(xhist$Inf_prop, xhist$NA_prop)[which_checks]/bdiff
         }
       }
-      
+
       ylim <- c(0, if (freq) {
         max(phist$counts, xhist$Inf_freq, xhist$NA_freq)
       } else {
         max(phist$density, xhist$Inf_prop/bdiff, xhist$NA_prop/bdiff)
       })
-      
-      plot(phist, 
+
+      plot(phist,
            freq = freq,
-           main = main, 
+           main = main,
            xlab = xlab,
            ylab = ylab,
-           xlim = xlim, 
-           ylim = ylim, 
-           xaxt = 'n', 
-           col = col[1], 
-           border = border[1], 
+           xlim = xlim,
+           ylim = ylim,
+           xaxt = 'n',
+           col = col[1],
+           border = border[1],
            las = las,
            ...)
-      
+
       xticks <- pretty(brange)
       xticks <- xticks[which(xticks > brange[1] - 0.03 * diff(brange))]
       xticks <- xticks[which(xticks < brange[2] + 0.03 * diff(brange))]
       axis(side = 1, at = xticks, las = las)
-      
+
       if (n_checks > 0L) {
         rect(
           xleft = check_xleft,
@@ -177,10 +184,10 @@ plot.pith <- function(
           col = col[2],
           border = border[2])
         axis(
-          side = 1, 
-          at = 0.5 * (check_xleft + check_xright), 
-          labels = c("\U00b1\nInf", "NA\nNaN")[which_checks], 
-          tick = FALSE, 
+          side = 1,
+          at = 0.5 * (check_xleft + check_xright),
+          labels = c("\U00b1\nInf", "NA\nNaN")[which_checks],
+          tick = FALSE,
           las = las)
       }
     }
